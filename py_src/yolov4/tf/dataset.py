28fe154fb321068dcb93b35d8a5e4f4f6465da9f
Hyeonki Hong <hhk7734@gmail.com>
2020-07-10 17:02:33 +0900

    utility: dataset: move to tf.dataset

--- py_src/yolov4/tf/dataset.py
+++ py_src/yolov4/tf/dataset.py
@@ -23,13 +23,12 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-
 import os
 import cv2
 import numpy as np
 
-from . import media
 from . import train
+from ..utility import media
 
 
 class Dataset:
