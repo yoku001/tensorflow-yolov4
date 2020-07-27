e5c9159704380947a305af4695c8edd97223451b
Hyeonki Hong <hhk7734@gmail.com>
2020-07-27 17:47:20 +0900

    tf: train: remove weight for conf_noobj_loss

--- py_src/yolov4/tf/train.py
+++ py_src/yolov4/tf/train.py
@@ -124,7 +124,6 @@ class YOLOv4Loss(Loss):
         conf_obj_loss = one_obj * (0.0 - backend.log(pred_conf + 1e-9))
         conf_noobj_loss = (
             one_noobj
-            * 0.5
             * tf.cast(max_iou < 0.5, dtype=tf.float32)
             * (0.0 - backend.log(1.0 - pred_conf + 1e-9))
         )
