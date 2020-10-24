da2531b69f4e4a2db854b5047b0e175cca4fc158
Hyeonki Hong <hhk7734@gmail.com>
2020-10-25 02:25:34 +0900

    tf:dataset: reflect input_size type change to data augmentation

--- py_src/yolov4/tf/dataset.py
+++ py_src/yolov4/tf/dataset.py
@@ -209,7 +209,10 @@ class Dataset:
                 iou_mask = iou > 0.3
 
                 if np.any(iou_mask):
-                    xy_grid = xywh[0:2] * self.grid_size[i]
+                    xy_grid = xywh[0:2] * (
+                        self.grid_size[i][1],
+                        self.grid_size[i][0],
+                    )
                     xy_index = np.floor(xy_grid)
 
                     exist_positive = True
@@ -225,7 +228,10 @@ class Dataset:
                 i = index // 3
                 j = index % 3
 
-                xy_grid = xywh[0:2] * self.grid_size[i]
+                xy_grid = xywh[0:2] * (
+                    self.grid_size[i][1],
+                    self.grid_size[i][0],
+                )
                 xy_index = np.floor(xy_grid)
 
                 _x, _y = int(xy_index[0]), int(xy_index[1])
@@ -331,13 +337,15 @@ class Dataset:
 
 def cut_out(dataset):
     """
-    @parma dataset: image(float), bboxes
+    @parma `dataset`: [image(float), bboxes]
             bboxes = [image_path, [[x, y, w, h, class_id], ...]]
     """
-    _size = dataset[0].shape[1]
+    _size = dataset[0].shape[1:3]  # height, width
     for bbox in dataset[1]:
         if random.random() < 0.5:
-            _pixel_bbox = [int(pos * _size) for pos in bbox[0:4]]
+            _pixel_bbox = [
+                int(pos * _size[(i + 1) % 2]) for i, pos in enumerate(bbox[0:4])
+            ]
             _x_min = _pixel_bbox[0] - (_pixel_bbox[2] // 2)
             _y_min = _pixel_bbox[1] - (_pixel_bbox[3] // 2)
             _cut_out_width = _pixel_bbox[2] // 4
@@ -368,39 +376,39 @@ def mix_up(dataset0, dataset1, alpha=0.2):
 
 
 def mosaic(dataset0, dataset1, dataset2, dataset3):
-    size = dataset0[0].shape[1]
-    image = np.empty((1, size, size, 3))
+    size = dataset0[0].shape[1:3]  # height, width
+    image = np.empty((1, size[0], size[1], 3))
     bboxes = []
 
-    partition_x = int((random.random() * 0.6 + 0.2) * size)
-    partition_y = int((random.random() * 0.6 + 0.2) * size)
+    partition_x = int((random.random() * 0.6 + 0.2) * size[1])
+    partition_y = int((random.random() * 0.6 + 0.2) * size[0])
 
     x_offset = [0, partition_x, 0, partition_x]
     y_offset = [0, 0, partition_y, partition_y]
 
     left = [
-        (size - partition_x) // 2,
+        (size[1] - partition_x) // 2,
         partition_x // 2,
-        (size - partition_x) // 2,
+        (size[1] - partition_x) // 2,
         partition_x // 2,
     ]
     right = [
         left[0] + partition_x,
-        left[1] + size - partition_x,
+        left[1] + size[1] - partition_x,
         left[2] + partition_x,
-        left[3] + size - partition_x,
+        left[3] + size[1] - partition_x,
     ]
     top = [
-        (size - partition_y) // 2,
-        (size - partition_y) // 2,
+        (size[0] - partition_y) // 2,
+        (size[0] - partition_y) // 2,
         partition_y // 2,
         partition_y // 2,
     ]
     down = [
         top[0] + partition_y,
         top[1] + partition_y,
-        top[2] + size - partition_y,
-        top[3] + size - partition_y,
+        top[2] + size[0] - partition_y,
+        top[3] + size[0] - partition_y,
     ]
 
     image[:, :partition_y, :partition_x, :] = dataset0[0][
@@ -432,7 +440,9 @@ def mosaic(dataset0, dataset1, dataset2, dataset3):
         (dataset0[1], dataset1[1], dataset2[1], dataset3[1])
     ):
         for bbox in _bboxes:
-            pixel_bbox = bbox[0:4] * size
+            pixel_bbox = [
+                int(pos * size[(i + 1) % 2]) for i, pos in enumerate(bbox[0:4])
+            ]
             x_min = int(pixel_bbox[0] - pixel_bbox[2] // 2)
             y_min = int(pixel_bbox[1] - pixel_bbox[3] // 2)
             x_max = int(pixel_bbox[0] + pixel_bbox[2] // 2)
@@ -473,10 +483,10 @@ def mosaic(dataset0, dataset1, dataset2, dataset3):
                 np.array(
                     [
                         [
-                            ((x_min + x_max) / 2 + x_offset[i]) / size,
-                            ((y_min + y_max) / 2 + y_offset[i]) / size,
-                            (x_max - x_min) / size,
-                            (y_max - y_min) / size,
+                            ((x_min + x_max) / 2 + x_offset[i]) / size[1],
+                            ((y_min + y_max) / 2 + y_offset[i]) / size[0],
+                            (x_max - x_min) / size[1],
+                            (y_max - y_min) / size[0],
                             class_id,
                         ],
                     ]
