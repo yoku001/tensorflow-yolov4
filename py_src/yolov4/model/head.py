504fd4700e288bb8d1dc287234ccb8d818271114
Hyeonki Hong <hhk7734@gmail.com>
2020-10-26 03:13:12 +0900

    model: head: reflect input_size type change

--- py_src/yolov4/model/head.py
+++ py_src/yolov4/model/head.py
@@ -33,7 +33,7 @@ class YOLOv3Head(Model):
         self.anchors = anchors
         self.grid_coord = []
         self.grid_size = None
-        self.image_size = None
+        self.image_width = None
         self.num_classes = num_classes
         self.scales = xysclaes
 
@@ -46,31 +46,52 @@ class YOLOv3Head(Model):
         self.concat2 = layers.Concatenate(axis=-1)
 
     def build(self, input_shape):
-        grid = (input_shape[0][1], input_shape[1][1], input_shape[2][1])
+        # None, g_height, g_width,
+        #       (xywh + conf + num_classes) * (# of anchors)
 
-        self.reshape0.target_shape = (grid[0], grid[0], 3, 5 + self.num_classes)
-        self.reshape1.target_shape = (grid[1], grid[1], 3, 5 + self.num_classes)
-        self.reshape2.target_shape = (grid[2], grid[2], 3, 5 + self.num_classes)
+        # g_width, g_height
+        _size = [(shape[2], shape[1]) for shape in input_shape]
+
+        self.reshape0.target_shape = (
+            _size[0][1],
+            _size[0][0],
+            3,
+            5 + self.num_classes,
+        )
+        self.reshape1.target_shape = (
+            _size[1][1],
+            _size[1][0],
+            3,
+            5 + self.num_classes,
+        )
+        self.reshape2.target_shape = (
+            _size[2][1],
+            _size[2][0],
+            3,
+            5 + self.num_classes,
+        )
 
         self.a_half = [
             tf.constant(
                 0.5,
                 dtype=tf.float32,
-                shape=(1, grid[i], grid[i], 3, 2),
+                shape=(1, _size[i][1], _size[i][0], 3, 2),
             )
             for i in range(3)
         ]
 
         for i in range(3):
-            xy_grid = tf.meshgrid(tf.range(grid[i]), tf.range(grid[i]))
+            xy_grid = tf.meshgrid(tf.range(_size[i][0]), tf.range(_size[i][1]))
             xy_grid = tf.stack(xy_grid, axis=-1)
             xy_grid = xy_grid[tf.newaxis, :, :, tf.newaxis, :]
             xy_grid = tf.tile(xy_grid, [1, 1, 1, 3, 1])
             xy_grid = tf.cast(xy_grid, tf.float32)
             self.grid_coord.append(xy_grid)
 
-        self.grid_size = grid
-        self.image_size = grid[0] * 8
+        self.grid_size = tf.convert_to_tensor(_size, dtype=tf.float32)
+        self.image_width = tf.convert_to_tensor(
+            _size[0][0] * 8.0, dtype=tf.float32
+        )
 
     def call(self, x):
         raw_s, raw_m, raw_l = x
@@ -107,9 +128,9 @@ class YOLOv3Head(Model):
         prob_m = activations.sigmoid(prob_m)
         prob_l = activations.sigmoid(prob_l)
 
-        bwbh_s = (self.anchors[0] / self.image_size) * backend.exp(twth_s)
-        bwbh_m = (self.anchors[1] / self.image_size) * backend.exp(twth_m)
-        bwbh_l = (self.anchors[2] / self.image_size) * backend.exp(twth_l)
+        bwbh_s = (self.anchors[0] / self.image_width) * backend.exp(twth_s)
+        bwbh_m = (self.anchors[1] / self.image_width) * backend.exp(twth_m)
+        bwbh_l = (self.anchors[2] / self.image_width) * backend.exp(twth_l)
 
         pred_s = self.concat0([bxby_s, bwbh_s, conf_s, prob_s])
         pred_m = self.concat1([bxby_m, bwbh_m, conf_m, prob_m])
@@ -125,15 +146,21 @@ class YOLOv3HeadTiny(Model):
         self.anchors = tf.convert_to_tensor(anchors, dtype=tf.float32)
         self.grid_coord = []
         self.grid_size = None
-        self.image_size = None
+        self.image_width = None
         self.num_classes = num_classes
         self.scales = xysclaes
 
     def build(self, input_shape):
-        _size = [shape[1] for shape in input_shape]
+        # input_shape == None, g_height, g_width,
+        #                      (xywh + conf + num_classes) * (# of anchors)
+
+        # g_width, g_height
+        _size = [(shape[2], shape[1]) for shape in input_shape]
 
         for i in range(2):
-            xy_grid = np.meshgrid(np.arange(_size[i]), np.arange(_size[i]))
+            xy_grid = np.meshgrid(
+                np.arange(_size[i][0]), np.arange(_size[i][1])
+            )
             xy_grid = np.stack(xy_grid, axis=-1)
             xy_grid = xy_grid[np.newaxis, ...]
             self.grid_coord.append(
@@ -141,8 +168,8 @@ class YOLOv3HeadTiny(Model):
             )
 
         self.grid_size = tf.convert_to_tensor(_size, dtype=tf.float32)
-        self.image_size = tf.convert_to_tensor(
-            _size[0] * 16.0, dtype=tf.float32
+        self.image_width = tf.convert_to_tensor(
+            _size[0][0] * 16.0, dtype=tf.float32
         )
 
     def call(self, x):
@@ -151,7 +178,7 @@ class YOLOv3HeadTiny(Model):
         sig_m = activations.sigmoid(raw_m)
         sig_l = activations.sigmoid(raw_l)
 
-        # Dim(batch, grid, grid, 5 + num_classes)
+        # Dim(batch, g_height, g_width, 5 + num_classes)
         sig_m = tf.split(sig_m, 3, axis=-1)
         raw_m = tf.split(raw_m, 3, axis=-1)
         sig_l = tf.split(sig_l, 3, axis=-1)
@@ -162,7 +189,7 @@ class YOLOv3HeadTiny(Model):
             _, twth_m, _ = tf.split(raw_m[i], (2, 2, -1), axis=-1)
             txty_m = (txty_m - 0.5) * self.scales[0] + 0.5
             bxby_m = (txty_m + self.grid_coord[0]) / self.grid_size[0]
-            bwbh_m = (self.anchors[0][i] / self.image_size) * backend.exp(
+            bwbh_m = (self.anchors[0][i] / self.image_width) * backend.exp(
                 twth_m
             )
             sig_m[i] = tf.concat([bxby_m, bwbh_m, conf_prob_m], axis=-1)
@@ -171,12 +198,12 @@ class YOLOv3HeadTiny(Model):
             _, twth_l, _ = tf.split(raw_l[i], (2, 2, -1), axis=-1)
             txty_l = (txty_l - 0.5) * self.scales[1] + 0.5
             bxby_l = (txty_l + self.grid_coord[1]) / self.grid_size[1]
-            bwbh_l = (self.anchors[1][i] / self.image_size) * backend.exp(
+            bwbh_l = (self.anchors[1][i] / self.image_width) * backend.exp(
                 twth_l
             )
             sig_l[i] = tf.concat([bxby_l, bwbh_l, conf_prob_l], axis=-1)
 
-        # Dim(batch, grid, grid, 3 * (5 + num_classes))
+        # Dim(batch, g_height, g_width, 3 * (5 + num_classes))
         pred_m = tf.concat(sig_m, axis=-1)
         pred_l = tf.concat(sig_l, axis=-1)
 
